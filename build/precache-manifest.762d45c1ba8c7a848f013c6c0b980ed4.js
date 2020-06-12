829b2d34466542b455e3cc818048469149a79117
usarneme <jamestcoop@gmail.com>
2020-06-12 14:34:39 -0700

    Added build to git for pushing production builds to Heroku. Updated package.json to latest versions without breaking changes. Remade node_modules and package-lock. Added a proxy object to package.json which points to the Express server which is running the backend as well as serving the static html/css/js assets within this project build/ folder.

---
+++ build/precache-manifest.762d45c1ba8c7a848f013c6c0b980ed4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "035634ab907d2ef2198261ff42112dec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d2de3b8594203dcf6da",
+    "url": "/static/css/main.7034ae8e.chunk.css"
+  },
+  {
+    "revision": "d1b7b879e42fddd47da8",
+    "url": "/static/js/2.4d4705de.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/static/js/2.4d4705de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d2de3b8594203dcf6da",
+    "url": "/static/js/main.fcde47eb.chunk.js"
+  },
+  {
+    "revision": "7139a04db9326dd7fc23",
+    "url": "/static/js/runtime-main.b9e9cb75.js"
+  },
+  {
+    "revision": "e5a41a1fb16637927922789f450eb11d",
+    "url": "/static/media/account.e5a41a1f.svg"
+  },
+  {
+    "revision": "48030e753649d75af55c67353fec597d",
+    "url": "/static/media/add.48030e75.svg"
+  },
+  {
+    "revision": "a918556d0b220c9f667051965922a209",
+    "url": "/static/media/addLog.a918556d.svg"
+  },
+  {
+    "revision": "bd86eac2eeeb0164b5ba1d773ae24ff6",
+    "url": "/static/media/editPencil.bd86eac2.svg"
+  },
+  {
+    "revision": "d6ad0c97fce1c3e8651e15c8e70e70c3",
+    "url": "/static/media/home.d6ad0c97.svg"
+  },
+  {
+    "revision": "0476682c50f4dc09e14eeaa5b1d00482",
+    "url": "/static/media/log.0476682c.svg"
+  },
+  {
+    "revision": "369e264bad80bee0321c28b8a4915616",
+    "url": "/static/media/magnifying.369e264b.svg"
+  },
+  {
+    "revision": "fb5ad7c73e372f4c2e8119ca41920967",
+    "url": "/static/media/search.fb5ad7c7.svg"
+  },
+  {
+    "revision": "91e3307a602423a3a16af1bc8354e577",
+    "url": "/static/media/todo.91e3307a.svg"
+  }
+]);
